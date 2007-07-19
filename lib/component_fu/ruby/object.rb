921ff3b79253655f27b2f2c5eb76e330d7e51423
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2007-07-19 19:08:30 +0000

    Implemented Object#load override.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/component_fu/trunk@378 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/component_fu/ruby/object.rb
+++ lib/component_fu/ruby/object.rb
@@ -1,16 +1,29 @@
 class Object
   def require(file)
+    __component_fu_get_file(file) do |file|
+      super file
+    end
+  end
+
+  def load(file)
+    __component_fu_get_file(file) do |file|
+      super file
+    end
+  end
+
+  private
+  def __component_fu_get_file(file)
     component_fu_file_exists = false
     ComponentFu::ComponentManager.load_paths.each do |path|
       full_path = File.join(path, File.basename(file))
       full_path_rb = "#{full_path}.rb"
       if File.exists?(full_path_rb)
         component_fu_file_exists = true
-        super(full_path_rb)
+        yield(full_path_rb)
       end
     end
     begin
-      super
+      yield(file)
     rescue Exception => e
       raise(e) unless component_fu_file_exists
     end
--- spec/component_fu/object_spec.rb
+++ spec/component_fu/object_spec.rb
@@ -4,6 +4,9 @@ describe Object, "#require" do
   it_should_behave_like "ComponentFu::ComponentManager fixture"
   
   before do
+    if Object.const_defined?(:RetardedHelper)
+      Object.send(:remove_const, :RetardedHelper)
+    end
     @fixture = Object.new
   end
   
@@ -12,6 +15,15 @@ describe Object, "#require" do
     @manager.components << plugin_root
 
     @fixture.require 'retarded_helper'
+
+    Object.const_defined?(:RetardedHelper).should == true
+  end
+
+  it "loads Compentized plugins" do
+    plugin_root = "#{RAILS_ROOT}/vendor/plugins/acts_as_retarded"
+    @manager.components << plugin_root
+
+    @fixture.load 'retarded_helper'
 
     Object.const_defined?(:RetardedHelper).should == true
   end
