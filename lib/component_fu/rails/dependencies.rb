6b98a9b440a2e2f0696d79cac8578405d44f98b7
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2007-07-20 22:12:46 +0000

    PT/BT - Refactoring.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/component_fu/trunk@404 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/component_fu/rails/dependencies.rb
+++ lib/component_fu/rails/dependencies.rb
@@ -11,17 +11,7 @@ module Dependencies
     end
 
     unless from_mod.const_defined?(const_name)
-      parent_mod = from_mod.parent
-      if from_mod.parents.any? {|p| p.const_defined?(const_name)}
-        raise "Constant #{from_mod}::#{const_name} Not found"
-      end
-      if parent_mod != from_mod
-        return parent.const_missing(const_name)
-      end
-      from_mod.const_set(const_name, Module.new)
-      unless autoloaded_constants.include?(qualified_name)
-        autoloaded_constants << qualified_name
-      end
+      define_missing_constant from_mod, const_name, qualified_name
     end
     from_mod.const_get(const_name)
   end
@@ -32,17 +22,18 @@ module Dependencies
     return if load_once_path?(file)
     autoloaded_constants << qualified_name
   end
+
+  def define_missing_constant(from_mod, const_name, qualified_name)
+    parent_mod = from_mod.parent
+    if from_mod.parents.any? {|parent| parent.const_defined?(const_name)}
+      raise "Constant #{from_mod}::#{const_name} Not found"
+    end
+    if parent_mod != from_mod
+      return parent.const_missing(const_name)
+    end
+    from_mod.const_set(const_name, Module.new)
+    unless autoloaded_constants.include?(qualified_name)
+      autoloaded_constants << qualified_name
+    end
+  end
 end
-#
-#if (parent = from_mod.parent) && parent != from_mod &&
-#    ! from_mod.parents.any? { |p| p.const_defined?(const_name) }
-## If our parents do not have a constant named +const_name+ then we are free
-## to attempt to load upwards. If they do have such a constant, then this
-## const_missing must be due to from_mod::const_name, which should not
-## return constants from from_mod's parents.
-#begin
-#  return parent.const_missing(const_name)
-#rescue NameError => e
-#  raise unless e.missing_name? qualified_name_for(parent, const_name)
-#  raise name_error
-#end
