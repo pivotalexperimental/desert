5212eb7efdb51d0c6db3a585ea9732551b01fc4b
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2007-07-20 18:13:33 +0000

    Reformatting
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/component_fu/trunk@395 af276e61-6b34-4dac-905b-574b5f35ef33

--- spec/spec_suite.rb
+++ spec/spec_suite.rb
@@ -1,7 +1,6 @@
 class SpecSuite
   def run
     dir = File.dirname(__FILE__)
-
     system("ruby #{dir}/unit_spec_suite.rb") || raise("Unit Spec Suite failed")
     system("ruby #{dir}/rails_root/spec/rails_spec_suite.rb") || raise("Rails Spec Suite failed")
   end
