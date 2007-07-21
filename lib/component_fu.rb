87428c4352bda2b3e32f805d45d06d72aa6d771c
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2007-07-21 01:53:18 +0000

    Organizing files. Requiring plugin migrations.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/component_fu/trunk@413 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/component_fu.rb
+++ lib/component_fu.rb
@@ -1,5 +1,6 @@
 require "active_support"
 require "action_controller"
+require "active_record"
 
 require "component_fu/component_manager"
 require "component_fu/rails/initializer"
@@ -7,3 +8,8 @@ require "component_fu/rails/dependencies"
 require "component_fu/ruby/object"
 
 require "component_fu/rails/route_set"
+
+require "component_fu/plugin_migrations/plugin_migrations"
+require "component_fu/plugin_migrations/migrator"
+require "component_fu/plugin_migrations/extensions/plugin"
+require "component_fu/plugin_migrations/extensions/schema_statements"
---
+++ lib/component_fu/plugin_migrations/migrator.rb
@@ -0,0 +1,43 @@
+module PluginAWeek #:nodoc:
+  module PluginMigrations
+    # Responsible for migrating plugins.  PluginAWeek::PluginMigrations.Migrator.current_plugin
+    # indicates which plugin is currently being migrated
+    class Migrator < ActiveRecord::Migrator
+      # We need to be able to set the current plugin being migrated.
+      cattr_accessor :current_plugin
+
+      class << self
+        # Runs the migrations from a plugin, up (or down) to the version given
+        def migrate_plugin(plugin, version = nil)
+          self.current_plugin = plugin
+          migrate(plugin.migration_path, version)
+        end
+
+        def schema_info_table_name #:nodoc:
+          ActiveRecord::Base.table_name_prefix + 'plugin_schema_info' + ActiveRecord::Base.table_name_suffix
+        end
+
+        def current_version #:nodoc:
+          result = ActiveRecord::Base.connection.select_one("SELECT version FROM #{schema_info_table_name} WHERE plugin_name = '#{current_plugin.name}'")
+          if result
+            result['version'].to_i
+          else
+            # There probably isn't an entry for this plugin in the migration info table.
+            0
+          end
+        end
+      end
+
+      def set_schema_version(version)
+        version = down? ? version.to_i - 1 : version.to_i
+
+        if ActiveRecord::Base.connection.select_one("SELECT version FROM #{self.class.schema_info_table_name} WHERE plugin_name = '#{current_plugin.name}'").nil?
+          # We need to create the entry since it doesn't exist
+          ActiveRecord::Base.connection.execute("INSERT INTO #{self.class.schema_info_table_name} (version, plugin_name) VALUES (#{version},'#{current_plugin.name}')")
+        else
+          ActiveRecord::Base.connection.update("UPDATE #{self.class.schema_info_table_name} SET version = #{version} WHERE plugin_name = '#{current_plugin.name}'")
+        end
+      end
+    end
+  end
+end
