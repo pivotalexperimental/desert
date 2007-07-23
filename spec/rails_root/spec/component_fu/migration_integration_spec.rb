3533ae02912c3d82792bd9da8562ae283fa89321
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2007-07-23 23:00:44 +0000

    PT/BT - Added specs verifying that migrations are working.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/component_fu/trunk@522 af276e61-6b34-4dac-905b-574b5f35ef33

--- spec/rails_root/spec/component_fu/migration_integration_spec.rb
+++ spec/rails_root/spec/component_fu/migration_integration_spec.rb
@@ -13,15 +13,26 @@ describe Migration, "#migrate_plugin"do
 
   it "creates plugin schema info table" do
     Migration.migrate_plugin('acts_as_spiffy', 2)
-    results = []
-    execute("select * from plugin_schema_info;").each_hash do |hash|
-      results << hash
-    end
+    results = select("select * from plugin_schema_info;")
     results.should == [
       {
         'plugin_name' => 'acts_as_spiffy',
         'version' => '2'
       },
+    ]
+  end
+
+  it "migrates the data" do
+    Migration.migrate_plugin('acts_as_spiffy', 2)
+    tables = select("show tables")
+    table_names = tables.collect do |row|
+      row.values.first
+    end
+    table_names.should == [
+      "companies",
+      "employees",
+      "plugin_schema_info",
+      "schema_info"
     ]
   end
 
@@ -32,14 +43,24 @@ describe Migration, "#migrate_plugin"do
   end
 
   def drop_tables
-    execute("show tables").each_hash do |hash|
-      table_name = hash.values.first
+    tables = select("show tables")
+    tables.each do |row|
+      table_name = row.values.first
       execute "drop table `#{table_name}`"
     end
   end
 
+  def select(sql)
+    result_set = []
+    stream = execute(sql)
+    stream.each_hash do |hash|
+      result_set << hash
+    end
+    result_set
+  end
+
   def execute(sql)
-    Base.connection.execute sql
+    Base.connection.execute(sql)
   end
 end
 end
