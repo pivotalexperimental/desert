c01addaf619698f2a3cb8a797760c2fd9183a7b4
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2007-07-24 22:03:02 +0000

    Rename directory
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/desert/trunk@549 af276e61-6b34-4dac-905b-574b5f35ef33

--- spec/unit_spec_suite.rb
+++ spec/unit_spec_suite.rb
@@ -1,7 +1,7 @@
 class UnitSpecSuite
   def run
     dir = File.dirname(__FILE__)
-    Dir["#{dir}/component_fu/**/*_spec.rb"].each do |file|
+    Dir["#{dir}/desert/**/*_spec.rb"].each do |file|
       require file
     end
   end
