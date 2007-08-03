fa280d8c43dba1e7d981776f8b668e9e6f014c94
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2007-08-03 00:51:40 +0000

    Added Migrator#table_exists? and Migrator#column_exists?
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/desert/trunk@605 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/desert/rails/migration.rb
+++ lib/desert/rails/migration.rb
@@ -20,6 +20,23 @@ class ActiveRecord::Migration
       return plugin if plugin
       raise ArgumentError, "No plugin found named #{plugin_name}"
     end
+
+    def table_exists?(table_name)
+      vals = select_all("DESC #{table_name}")
+      return true
+    rescue ActiveRecord::StatementInvalid
+      return false
+    end
+
+    def column_exists?(table_name, column_name)
+      val = select_one("select #{column_name} from #{table_name}")
+      return true
+    rescue ActiveRecord::StatementInvalid
+      return false
+    end
+    def table_exists?(table_name)
+      ActiveRecord::Base.connection.tables.include? table_name
+    end
   end
   extend DesertMigration
 end
--- spec/desert/rails/migration_spec.rb
+++ spec/desert/rails/migration_spec.rb
@@ -1,12 +1,29 @@
 require "spec/spec_helper"
 
-module ActiveRecord
-class Migration
-describe DesertMigration, "#migrate_plugin" do
-  it_should_behave_like "Desert::Manager fixture"
-end
+describe ActiveRecord::Migration::DesertMigration do
+  it "test_plugin_for__with_invalid_name__raises_exception" do
+    proc do
+      path_for_plugin("non_existent_plugin")
+    end.should raise_error
+  end
 
-describe DesertMigration, "#schema_version_equivalent_to" do
-end
-end
+  it "test_migrate_plugin" do
+    fake_plugin = "i am a plugin"
+    stub(Desert::Manager).find_plugin("my_plugin") {fake_plugin}
+    mock(PluginAWeek::PluginMigrations::Migrator).migrate_plugin(fake_plugin, 3)
+
+    migrate_plugin("my_plugin", 3)
+  end
+
+  it "test_schema_version_equivalent_to" do
+    fake_plugin = "i am a plugin"
+    fake_migrator = "fake migrator"
+    stub(Desert::Manager).find_plugin("my_plugin") {fake_plugin}
+    
+    mock(PluginAWeek::PluginMigrations::Migrator).allocate {fake_migrator}
+    mock(fake_migrator).set_schema_version(3)
+
+    schema_version_equivalent_to("my_plugin", 3)
+    PluginAWeek::PluginMigrations::Migrator.current_plugin.should == fake_plugin
+  end
 end
