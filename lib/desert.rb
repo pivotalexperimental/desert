6e4d2ff8761cc05cd126d3c3af82c1c0c0e18e74
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-04-26 20:17:57 +0000

    Ignoring idea files.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/desert/trunk@1493 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/desert.rb
+++ lib/desert.rb
@@ -3,6 +3,14 @@ require "active_record"
 require "action_controller"
 require "action_mailer"
 
+module Desert
+  SUPPORTED_RAILS_VERSIONS = {
+    "1.2.5" => {'version' => '1.2.5', 'git_tag' => 'v1.2.5'},
+    "1.99.0" => {'version' => '1.99.0', 'git_tag' => 'v2.0.0_RC1'},
+    "2.0.2" => {'version' => '2.0.2', 'git_tag' => 'v2.0.2'},
+  }
+end
+
 dir = File.dirname(__FILE__)
 require "#{dir}/desert/plugin"
 require "#{dir}/desert/manager"
