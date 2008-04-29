937c5f37c750dd53cd10c789711c8d98e2d203f4
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-04-29 21:48:20 +0000

    Fix Rails Desert version issue.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/desert/trunk@1536 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/desert/plugin_templates.rb
+++ lib/desert/plugin_templates.rb
@@ -1,6 +1,6 @@
 dir = File.dirname(__FILE__)
 require "#{dir}/plugin_templates/action_controller"
-if Desert::VersionChecker.rails_version_is_below_rc2?
+if Desert::VersionChecker.rails_version_is_below_1990?
   require "#{dir}/plugin_templates/1.2.0/action_mailer"
 else
   require "#{dir}/plugin_templates/2.0.0/action_mailer"
