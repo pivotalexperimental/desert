d125297d425acb08f53e0260c7c6d9e441962a0c
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-05-24 03:47:58 +0000

    Fixed template rendering for multiple template extensions.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/desert/trunk@1586 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/desert/plugin_templates/1.99.0/action_mailer.rb
+++ lib/desert/plugin_templates/1.99.0/action_mailer.rb
@@ -2,23 +2,17 @@ module ActionMailer #:nodoc
   class Base #:nodoc:
     private
       def template_path_with_plugin_routing
-        result = nil
-        Desert::Manager.plugins_and_app.reverse.each do |plugin|
-          relative_path = "#{plugin.templates_path}/#{mailer_name}"
-          unless Dir["#{relative_path}/#{@template}.*"].empty?
-            result = relative_path
-            break
-          end
+        template_paths = [template_path_without_plugin_routing]
+        Desert::Manager.plugins.reverse.each do |plugin|
+          template_paths << "#{plugin.templates_path}/#{mailer_name}"
         end
-        result || template_path_without_plugin_routing
+        "{#{template_paths * ','}}"
       end
       alias_method_chain :template_path, :plugin_routing
 
       def initialize_template_class(assigns)
-        view_paths = Dir["#{template_path}/#{@template}.*"].collect do |path|
-          File.dirname(path)
-        end
-        returning(template = ActionView::Base.new(view_paths, assigns, self)) do
+        view_path = File.dirname(Dir["#{template_path}/#{@template}.*"].first)
+        returning(template = ActionView::Base.new([view_path], assigns, self)) do
           template.extend ApplicationHelper
           template.extend self.class.master_helper_module
         end
--- spec/rails_root/spec/desert/action_view_integration_spec.rb
+++ spec/rails_root/spec/desert/action_view_integration_spec.rb
@@ -2,22 +2,27 @@ dir = File.dirname(__FILE__)
 require "#{dir}/../rails_spec_helper"
 
 module ActionView
-describe Base, "#render partial" do
-  before do
-    @controller = ::Spiffy::SpiffyController.new
-    @request = ActionController::TestRequest.new
-    @response = ActionController::TestResponse.new
-    @controller.send(:initialize_template_class, @response)
-    @controller.send(:assign_shortcuts, @request, @response)
-    @base = ActionView::Base.new("#{RAILS_ROOT}/app/views", {}, @controller)
-  end
+  describe Base, "#render partial" do
+    before do
+      @controller = ::Spiffy::SpiffyController.new
+      @request = ActionController::TestRequest.new
+      @response = ActionController::TestResponse.new
+      @controller.send(:initialize_template_class, @response)
+      @controller.send(:assign_shortcuts, @request, @response)
+      if ActionView::Base.instance_methods.include?('base_path')
+        @base = ActionView::Base.new(["#{RAILS_ROOT}/app/views"], {}, @controller)
+      else
+        @base = ActionView::Base.new("#{RAILS_ROOT}/app/views", {}, @controller)
+      end
+    end
 
-  it "gives priority to the rails app" do
-    @base.render(:partial => "spiffy").should == "From App"
-  end
+    it "gives priority to the rails app" do
+      @controller.send(:render_to_string, :template => "spiffy/spiffy/spiffy").should == "From App"
+      @base.render(:partial => "spiffy").should == "From App"
+    end
 
-  it "gives priority to plugins added later" do
-    @base.render(:partial => "acts_as_spiffy").should == "From Super Spiffy"
+    it "gives priority to plugins added later" do
+      @base.render(:partial => "acts_as_spiffy").should == "From Super Spiffy"
+    end
   end
-end
 end
