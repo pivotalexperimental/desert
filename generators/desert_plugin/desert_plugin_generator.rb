048435c9750098fc32a191f662c4d2825b096c36
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-05-31 03:05:36 +0000

    updated readme, script/destroy now removes the route from config/routes.rb instead of adding it again
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/desert/trunk@1601 af276e61-6b34-4dac-905b-574b5f35ef33

--- generators/desert_plugin/desert_plugin_generator.rb
+++ generators/desert_plugin/desert_plugin_generator.rb
@@ -1,3 +1,6 @@
+require 'rails_generator'
+require 'rails_generator/commands'
+
 class DesertPluginGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
@@ -11,7 +14,7 @@ class DesertPluginGenerator < Rails::Generator::NamedBase
 
       m.directory "vendor/plugins/#{file_name}/config"
       m.template "routes.rb", "vendor/plugins/#{file_name}/config/routes.rb"
-      map_route_from_plugin(m)
+      m.map_route_from_plugin
 
       m.directory "vendor/plugins/#{file_name}/db"
       m.directory "vendor/plugins/#{file_name}/db/migrate"
@@ -26,15 +29,44 @@ class DesertPluginGenerator < Rails::Generator::NamedBase
       m.directory "vendor/plugins/#{file_name}/spec/views"
       m.file "spec_helper.rb", "vendor/plugins/#{file_name}/spec/spec_helper.rb" 
 
+      m.directory "vendor/plugins/#{file_name}/tasks"
+
       m.file "empty_file", "vendor/plugins/#{file_name}/init.rb"
     end
   end
 
-  def map_route_from_plugin(m)
-    logger.route "adding map.routes_from_plugin(:#{file_name}) to top of routes.rb"
-    sentinel = 'ActionController::Routing::Routes.draw do |map|'
-    m.gsub_file 'config/routes.rb', /(#{Regexp.escape(sentinel)})/mi do |match|
-      "#{match}\n    map.routes_from_plugin(:#{file_name})\n"
+end
+
+module Desert #:nodoc:
+  module Generator #:nodoc:
+    module Commands #:nodoc:
+      module Create
+        def map_route_from_plugin
+          logger.route "adding map.routes_from_plugin(:#{file_name}) to top of routes.rb"
+          sentinel = 'ActionController::Routing::Routes.draw do |map|'
+          gsub_file('config/routes.rb', /(#{Regexp.escape(sentinel)})/mi) do |match|
+            "#{match}\n  map.routes_from_plugin(:#{file_name})\n"
+          end
+        end
+      end
+    end
+
+    module Destroy
+      def map_route_from_plugin
+        look_for = "map.routes_from_plugin(:#{file_name})"
+        logger.route "removing map.routes_from_plugin(:#{file_name}) from routes.rb"
+        gsub_file 'config/routes.rb', /(#{Regexp.escape(look_for)})/mi, ''
+      end
+    end
+
+    module List
+      def map_route_from_plugin
+        logger.route "adding map.routes_from_plugin(:#{file_name}) to top of routes.rb"
+      end
     end
   end
 end
+
+Rails::Generator::Commands::Create.send   :include,  Desert::Generator::Commands::Create
+Rails::Generator::Commands::Destroy.send  :include,  Desert::Generator::Commands::Destroy
+Rails::Generator::Commands::List.send     :include,  Desert::Generator::Commands::List
