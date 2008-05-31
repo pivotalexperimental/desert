d7d78911e48528d77aeb7e8bc365fd8882ffe187
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-05-31 03:05:34 +0000

    don't add a route to the plugin's routes.rb - just an example route
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/desert/trunk@1600 af276e61-6b34-4dac-905b-574b5f35ef33

--- generators/desert_plugin/templates/routes.rb
+++ generators/desert_plugin/templates/routes.rb
@@ -1,1 +1,4 @@
-resources :<%= plural_name %>
+# Add your custom routes here.  If in config/routes.rb you would 
+# add <tt>map.resources</tt>, here you would add just <tt>resources</tt>
+
+# resources :<%= plural_name %>
