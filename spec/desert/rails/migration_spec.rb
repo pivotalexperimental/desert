6b946941cd5435cd8fb243b31e27285e9f171fcd
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-05-31 20:16:33 +0000

    Renaming specs for Migration.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/desert/trunk@1607 af276e61-6b34-4dac-905b-574b5f35ef33

--- spec/desert/rails/migration_spec.rb
+++ spec/desert/rails/migration_spec.rb
@@ -7,37 +7,36 @@ describe ActiveRecord::Migration::DesertMigration do
     fixture.extend ActiveRecord::Migration::DesertMigration
   end
 
-  it "test_plugin_for__with_invalid_name__raises_exception" do
-    proc do
-      path_for_plugin("non_existent_plugin")
-    end.should raise_error
+  context "when plugin has an invalid name" do
+    it "raises an error" do
+      proc do
+        path_for_plugin("non_existent_plugin")
+      end.should raise_error
+    end
   end
 
-  it "test_migrate_plugin" do
-    fake_plugin = "i am a plugin"
-    stub(Desert::Manager).find_plugin("my_plugin") {fake_plugin}
-    mock(Desert::PluginMigrations::Migrator).migrate_plugin(fake_plugin, 3)
+  describe "#migrate_plugin" do
+    it "migrates the particular plugin" do
+      fake_plugin = "i am a plugin"
+      stub(Desert::Manager).find_plugin("my_plugin") {fake_plugin}
+      mock(Desert::PluginMigrations::Migrator).migrate_plugin(fake_plugin, 3)
 
-    fixture.migrate_plugin("my_plugin", 3)
+      fixture.migrate_plugin("my_plugin", 3)
+    end
   end
 
-  it "test_migrate_plugin_with_symbol" do
-    fake_plugin = "i am a plugin"
-    stub(Desert::Manager).find_plugin('my_plugin') {fake_plugin}
-    mock(Desert::PluginMigrations::Migrator).migrate_plugin(fake_plugin, 3)
+  describe "#schema_version_equivalent_to" do
+    it "sets the current plugin to the plugin referenced by the migration" do
+      fake_plugin = "i am a plugin"
+      fake_migrator = "fake migrator"
+      stub(Desert::Manager).find_plugin("my_plugin") {fake_plugin}
 
-    fixture.migrate_plugin(:my_plugin, 3)
+      mock(Desert::PluginMigrations::Migrator).allocate {fake_migrator}
+      mock(fake_migrator).set_schema_version(3)
+
+      fixture.schema_version_equivalent_to("my_plugin", 3)
+      Desert::PluginMigrations::Migrator.current_plugin.should == fake_plugin
+    end
   end
 
-  it "test_schema_version_equivalent_to" do
-    fake_plugin = "i am a plugin"
-    fake_migrator = "fake migrator"
-    stub(Desert::Manager).find_plugin("my_plugin") {fake_plugin}
-    
-    mock(Desert::PluginMigrations::Migrator).allocate {fake_migrator}
-    mock(fake_migrator).set_schema_version(3)
-
-    fixture.schema_version_equivalent_to("my_plugin", 3)
-    Desert::PluginMigrations::Migrator.current_plugin.should == fake_plugin
-  end
 end
