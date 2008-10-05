027bc951b5cecbd220c035dab70bf9a5dce49357
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-10-05 21:04:41 +0000

    Ignoring schema.rb. Added development database.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/desert/trunk@1899 af276e61-6b34-4dac-905b-574b5f35ef33

--- spec/desert/rails/route_set_spec.rb
+++ spec/desert/rails/route_set_spec.rb
@@ -1,25 +1,27 @@
 require File.expand_path("#{File.dirname(__FILE__)}/../../spec_helper")
 
 module ActionController
-module Routing
-class RouteSet
-  describe Mapper, "#routes_from_plugin" do
-    it_should_behave_like "Desert::Manager fixture"
+  module Routing
+    class RouteSet
+      describe Mapper do
+        describe "#routes_from_plugin" do
+          it_should_behave_like "Desert::Manager fixture"
 
-    before do
-      @manager.register_plugin "#{RAILS_ROOT}/vendor/plugins/the_grand_poobah"
-    end
+          before do
+            @manager.register_plugin "#{RAILS_ROOT}/vendor/plugins/the_grand_poobah"
+          end
 
-    it "adds a named route from the plugin" do
-      map = ActionController::Routing::RouteSet::Mapper.new(nil)
-      mock(map).named_route(
-        "show_grand_poobah",
-        "/poobahs/grand/show",
-        {:controller => "grand_poobah", :action => "show"}
-      )
-      map.routes_from_plugin('the_grand_poobah')
+          it "adds a named route from the plugin" do
+            map = ActionController::Routing::RouteSet::Mapper.new(nil)
+            mock(map).named_route(
+            "show_grand_poobah",
+            "/poobahs/grand/show",
+            {:controller => "grand_poobah", :action => "show"}
+            )
+            map.routes_from_plugin('the_grand_poobah')
+          end
+        end
+      end
     end
   end
-end
-end
 end
