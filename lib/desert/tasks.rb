133b2deada381b290a4e030562442421f46b06e9
Brian Takita <btakita@btakita-laptop.(none)>
2008-12-09 02:10:12 -0500

    Fixed loading of desert tasks. Testing loading desert tasks in test rails app.

--- lib/desert/tasks.rb
+++ lib/desert/tasks.rb
@@ -1,4 +1,4 @@
 dir = File.dirname(__FILE__)
 Dir["#{dir}/tasks/*.rake"].each do |file|
-  require file
+  load file
 end
---
+++ spec/rails_root/lib/tasks/desert.rake
@@ -0,0 +1,2 @@
+$LOAD_PATH.unshift(File.expand_path("#{RAILS_ROOT}/../../lib"))
+require "desert/tasks"
